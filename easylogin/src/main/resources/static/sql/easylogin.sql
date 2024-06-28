9147b6a2d4e52023d801e007c47d4b7150b94053
rikukarim <katamarikari2000@gmail.com>
2024-06-28 12:48:23 +0900

    Lesson_EasyLogin

---
+++ easylogin/src/main/resources/static/sql/easylogin.sql
@@ -0,0 +1,14 @@
+create database boot1;
+
+use boot1;
+
+create table mst_user(
+id int not null Primary key auto_increment,
+user_name varchar(25) not null unique,
+password varchar(25) not null,
+full_name varchar(50) not null
+);
+
+insert into mst_user
+(user_name, password, full_name)
+values('taro', '111', 'Taro Yamada');
